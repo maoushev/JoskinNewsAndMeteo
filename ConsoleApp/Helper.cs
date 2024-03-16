f6855b6d35342130708edf3bb7d5031cf7031ba4
Magomed Aoushev <aoushev.magomed@hotmail.com>
2024-03-16 04:29:27 +0100

    Added WeatherApi and stored api in a .env file

--- ConsoleApp/Helper.cs
+++ ConsoleApp/Helper.cs
@@ -1,11 +1,10 @@
-using System;
-using System.ComponentModel.Design;
+using DotNetEnv;
 
 public static class Helper
 {
     public static void PrintMenu()
     {
-        Console.Clear();
+        Console.Clear(); //Clear the screen before writing to it
         Console.WriteLine("════════════════════════════════════════");
         Console.WriteLine("         Joskin Meteo and News           ");
         Console.WriteLine("════════════════════════════════════════");
@@ -13,7 +12,7 @@ public static class Helper
 
     public static void PrintMenuOptions(string[] menuOptions)
     {
-        for (int i = 0; i < menuOptions.Length; i++)
+        for (int i = 0; i < menuOptions.Length; i++) //This will go through a list of string and prit it to the screen
         {
             Console.WriteLine($"               {i + 1}. {menuOptions[i]}");
         }
@@ -33,12 +32,51 @@ public static class Helper
                 {
                     selection = int.Parse(choice);
                 }
-                catch (Exception)
+                catch (Exception) //here we catch the exception but as we do not use it we don't write a name like "ex"
                 {
                     Console.WriteLine("Format error, please enter a number.");
                 }
             }
         } while (selection < 1 || selection > max);
         return selection;
+    }
+
+    public static async Task GetWeather(string cityName) //this is async as we use async values and every async function returns task.
+    {
+
+        Env.Load(); // this is a package used to store sensible information
+
+        string? apiKey = Environment.GetEnvironmentVariable("API_KEY");
+
+        if (apiKey == null)
+            throw new IOException(); //if we can't open the file to get the api key
+
+        string countryName = "BE";
+
+        string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={cityName},{countryName}&appid={apiKey}&units=metric";
+
+
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode(); // if country not found
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            dynamic? weatherData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+            if (weatherData != null)
+            {
+                Helper.PrintMenu();
+                Console.WriteLine($"Weather in {weatherData.name}, {weatherData.sys.country}:");
+                Console.WriteLine($"Description: {weatherData.weather[0].description}");
+                Console.WriteLine($"Temperature: {weatherData.main.temp} °C");
+                Console.WriteLine($"Humidity: {weatherData.main.humidity}%");
+                Console.Write("Press enter to continue.");
+                Console.ReadLine();
+            }
+
+        }
+
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             int selection = 0;
-            while (true)
+
+            while (true) //infinite loop
             {
                 string[] menuOptions = { "Meteo", "News", "Both", "Exit" };
                 Helper.PrintMenu();
@@ -18,12 +17,38 @@ namespace ConsoleApp
                 switch (selection)
                 {
                     case 1:
+                        try
+                        {
+                            string? city = null;
+                            Helper.PrintMenu();
+                            Console.Write("Please input a city : ");
+                            do
+                            {
+                                city = Console.ReadLine();
+                                if (city == null)
+                                    Console.WriteLine("Please input a string.");
+                            } while (city == null);
+
+
+                            await Helper.GetWeather(city);
+                        }
+                        catch (HttpRequestException) //If the api can't find the city
+                        {
+                            Console.Write("City not found.Press enter to continue");
+                            Console.ReadLine();
+                        }
+                        catch (IOException) // if the .env file cannot be opened
+                        {
+                            Console.Write("An error occured.Press enter to continue");
+                            Console.ReadLine();
+                        }
                         break;
                     case 2:
                         break;
                     case 3:
                         break;
                     case 4:
+                        Environment.Exit(0);
                         break;
                 }
 
